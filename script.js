bdff17e908fe9f66a67c3d8f091547a771fab536
Lucas0601 <lucasba0601@gmail.com>
2023-09-11 18:19:07 -0300

    nova atualização

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 function createCloseButton(li) {
-    let span = document.createElement("SPAN");
+    let botaoFechar = document.createElement("BOTAOFECHAR");
     let txt = document.createTextNode("\u00D7");
 
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+    botaoFechar.className = "close";
+    botaoFechar.appendChild(txt);
+    li.appendChild(botaoFechar);
 
-    span.onclick = () => span.parentElement.style.display = "none";
+    botaoFechar.onclick = () => botaoFechar.parentElement.style.display = "none";
 }
+
+/*
+function novoPopup(){
+    var windows = window.open ('popup.html', "popup")
+
+}
+*/
 
 document.querySelectorAll('li').forEach(createCloseButton);
 
@@ -16,33 +23,26 @@ document.querySelector('ul').addEventListener('click', (e) => {
         e.target.classList.toggle('checked');
 });
 
-function add() {
-    // Obtém o valor do campo de entrada no formulário chamado "task" e remove espaos em branco no início e no fim 
+function botaoaddTasks() {
     let input_value = document.form_main.task.value.trim(); 
-   
-    // Verifica se o valor do campo de entrada não está vazio (ou seja, contém algum texto) 
+
     if (input_value !== '') { 
-    // Cria um novo elemento de lista (li)
+
     let li = document.createElement('LI');
 
-    // Cria um nó texto contendo o valor do campo de entrada
     let input_text = document.createTextNode(input_value);
-
-    // Adiciona o nó de texto como filho do elemento de lista (li)
+    
     li.appendChild(input_text);
-
-    // Encontra o primeiro elemento <ul> (lista não ordenada) no documento e adiciona o elemento de lista (li) a ele
+    
     document.querySelector('ul').appendChild(li);
-
-    //Lima o valor do campo de dentrada, tornando-o vazio novamente
+   
     document.form_main.task.value = "";
 
-    // Chama a função createCloseButton para criar um botão de fechar no novo elemenro de lista (li)
     createCloseButton(li);
 }
 }
 
-function myFunction() {
+function alterarTema() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
